02afd64e9922a16ddc0a7456074919b5eed8b981
syzhang <sigridzhang0603@163.com>
2019-05-21 15:45:15 +0800

    featutre: clear input after submit

--- src/app/components/AddTodoItem/index.js
+++ src/app/components/AddTodoItem/index.js
@@ -2,18 +2,40 @@ import React, { PureComponent } from 'react';
 import './index.css';
 
 export default class AddTodoItem extends PureComponent {
+  constructor(props) {
+    super(props)
+    this.state = {
+      content: ''
+    }
+  }
+
+  onChange = (event) => {
+    this.setState({
+      ...this.state,
+      content: event.target.value
+    })
+  }
+
 
   handleAddItem = (event) => {
     const {addItem} = this.props
     event.preventDefault()
-    addItem(this.refs.itemContent.value)
+
+    if (!!this.state.content) {
+      addItem(this.state.content)
+
+      this.setState({
+        ...this.state,
+        content: ''
+      })
+    }
   }
 
   render() {
     return (
       <form onSubmit={this.handleAddItem}>
         <div className="inline-input">
-          <input type="text" ref="itemContent" />
+          <input type="text" ref="itemContent" onChange={this.onChange} value={this.state.content} />
           <button type="submit" className="btn">Add</button>
         </div>
       </form>
