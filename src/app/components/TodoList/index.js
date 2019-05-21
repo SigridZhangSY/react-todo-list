5287f2a67348c088d405ef4b30c3984adae69caa
syzhang <sigridzhang0603@163.com>
2019-05-21 11:12:49 +0800

    feature: render list

--- src/app/components/TodoList/index.js
+++ src/app/components/TodoList/index.js
@@ -2,13 +2,37 @@ import React, { PureComponent } from 'react';
 import './index.css';
 
 export default class TodoList extends PureComponent {
-    
-    render() {
-        return (
-            <div className="wrapper">
-                <h3>Todo List</h3>
-                <div className="list-content"></div>
-            </div>
-        );
-    };
+  constructor(props) {
+    super(props)
+    this.state = {
+      list: [
+        {
+          id: 1,
+          content: 'reading Refactor'
+        },
+        {
+          id: 2,
+          content: 'yoga for 30min'
+        }
+      ]
+    }
+  }
+
+  render() {
+    return (
+      <div className="wrapper">
+        <h3>Todo List</h3>
+        <ul className="list-content">
+          {
+            this.state.list.map(item => (
+              <li className="list-item" key={item.id}>
+                <p className="index">{item.id}</p>
+                <p>{item.content}</p>
+              </li>
+            ))
+          }
+        </ul>
+      </div>
+    );
+  };
 }
